dcfcd65d9c6b4f9f491ce5c215855e38e56fceb1
Mark Edwards <109709559+mark-227-g@users.noreply.github.com>
2023-03-14 16:26:44 -0400

    after tutor help

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,76 @@
 /************************************** 
 References
 **************************************/
-var weatherURLKey="cd9964c300819532fb2997b8363c0542";
-var searchBtnEl = document.querySelector("#searchCity");
 
 /************************************** 
 global variables
 **************************************/
-
+var weatherURLKey="cd9964c300819532fb2997b8363c0542";
 
 /************************************** 
 buttons
 **************************************/
+var searchBtnEl = document.querySelector("#searchCity");
 
 /************************************** 
 classes
 **************************************/
-
 class WeatherAPI{
-    /*
-    city="";
-    state="";
-    country="";
-    limit=1;
-    */
+    /* --------------------------------
+    class constructor
+    -------------------------------- */
     constructor(key)
     {
         this.key=key;
     };
+
+    /* --------------------------------
+    return url for geo code  
+    by zip code
+    -------------------------------- */
     geoCodeURLbyZip(zipcode)
     {
         return("https://api.openweathermap.org/data/2.5/forecast?zip="+zipcode+"&units=imperial&appid="+this.key);
     };
-    geoCodeURL(city,state,country,limit=1)
+
+    /* --------------------------------
+    return url for geo code 
+    by city, state, country
+    -------------------------------- */
+    //geoCodeURL(city,state,country,limit=1)
+    geoCodeURL(city,limit=1)
     {
-        return("https://api.openweathermap.org/geo/1.0/direct?q="+city+","+state+","+country+"&limit="+limit+"&units=imperial&appid="+this.key);
+      //  return("https://api.openweathermap.org/geo/1.0/direct?q="+city+","+state+","+country+"&limit="+limit+"&units=imperial&appid="+this.key);
+      return("https://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit="+limit+"&units=imperial&appid="+this.key);
     };
+
+    /* --------------------------------
+    return url for current weather 
+    by latitute and longitute
+    -------------------------------- */
     weatherURL(lat,lon)
     {
         return("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&units=imperial&appid="+this.key);
     };
+
+    /* --------------------------------
+    return url for 5 day forecast
+    by latitute and longitute
+    -------------------------------- */
     weatherForcastURL(lat,lon)
     {
         return("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&cnt=40&units=imperial&appid="+this.key);
     };   
 };
+
+/* --------------------------------
+declare new weather class object 
+and pass in key
+-------------------------------- */
 let w = new WeatherAPI(weatherURLKey);
+var wLat=0;
+var wLon=0;
+
 /************************************** 
 functions
 **************************************/
@@ -60,35 +85,34 @@ function getWeatherIcon(id){
     var img="<img src="+url+" ></img>";
     return(img);
 }
-function wrapLI(text)
-{
-    return("<li>"+text+"</li>");
-}
+
+
 
 function ShowCurrentWeather(data)
 {
-document.getElementById("city").innerHTML=data.name+" ("+convertDT(data.dt).toLocaleDateString()+")"+getWeatherIcon(data.weather[0].icon);
+console.log(data);
+document.getElementById("city").innerHTML= `<h2>${data.name} (${convertDT(data.dt).toLocaleDateString()}) ${getWeatherIcon(data.weather[0].icon)}</h2>`;
 document.getElementById("temp").innerHTML="Temp: "+data.main.temp+" &#8457;";
-document.getElementById("wind").innerHTML="Wind: "+data.wind.speed+"&nbspMPH";
-document.getElementById("humidity").innerHTML="Humidity: "+data.main.humidity+"%";
+document.getElementById("wind").textContent="Wind: "+data.wind.speed+" MPH";
+document.getElementById("humidity").textContent="Humidity: "+data.main.humidity+"%";
 };
 
 
 function fillWeatherCards(data)
 {
-var weatherHTML="";
 var cardCount=1;
 for (var i = 0; i < data.cnt; i+=1) {
     if(convertDT(data.list[i].dt).getHours()==11)
     {
-        weatherHTML="<ul>" +
-            wrapLI(convertDT(data.list[i].dt).toLocaleDateString())+
-            wrapLI(getWeatherIcon(data.list[i].weather[0].icon))+
-            wrapLI("Temp: "+data.list[i].main.temp+" &#8457;") +
-            wrapLI("Wind: "+data.list[i].wind.speed+"&nbspMPH") +
-            wrapLI("Humidity: "+data.list[i].main.humidity+"%")+
-            "</ul>";
-        document.getElementById("forcastCard-"+cardCount).innerHTML=weatherHTML;
+        var ul=document.createElement("ul");
+        ul.innerHTML = `<li> ${convertDT(data.list[i].dt).toLocaleDateString()}</l1>
+        <li>${getWeatherIcon(data.list[i].weather[0].icon)}</li>
+        <li>Temp: ${data.list[i].main.temp} &#8457 </li>
+        <li>Wind: ${data.list[i].wind.speed} MPH </li>
+        <li> Humidity: ${data.list[i].main.humidity}%</li>
+        `;
+        document.getElementById("forcastCard-"+cardCount).innerHTML="";
+        document.getElementById("forcastCard-"+cardCount).append(ul);
         cardCount++;
     };
   }
@@ -97,7 +121,8 @@ for (var i = 0; i < data.cnt; i+=1) {
 
 function getWeather()
 {
-    fetch(w.weatherURL(40.735657,-74.1723667))
+    //fetch(w.weatherURL(40.735657,-74.1723667))
+    fetch(w.weatherURL(wLat,wLon))
         .then(function (response) {
             return response.json();
         })
@@ -108,7 +133,8 @@ function getWeather()
 
 function getForcast()
 {
-    fetch(w.weatherForcastURL(40.735657,-74.1723667))
+    //fetch(w.weatherForcastURL(40.735657,-74.1723667))
+    fetch(w.weatherForcastURL(wLat,wLon))
         .then(function (response) {
             return response.json();
         })
@@ -124,22 +150,26 @@ function getGeocode(city,state)
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
-            w.lat=data.lat;
-            w.lon=data.lon;
-            console.log(w.lat+" : "+ w.log);
+            //console.log(data)
+            wLat=data[0].lat;
+            wLon=data[0].lon;
+            //console.log(w.lat+" : "+ w.lon);
+            getWeather();
+            getForcast();
         });
 }
 
 function searchCity(event)
 {
+    event.preventDefault()
+    console.log(event.target.children);
     var searchTxt=document.getElementById("cityInput").value;
-    const searchValue= searchTxt.split(",");
-    if(searchValue.length==2)
+    //const searchValue= searchTxt.split(",");
+    if(searchTxt.length)
     {
-        w.city=searchValue[0].trim();
-        w.state=searchValue[1].trim();
-        getGeocode(w.city,w.state);
+        //w.city=searchValue[0].trim();
+        //w.state=searchValue[1].trim();
+        getGeocode(searchTxt);
     };
 
 
@@ -147,6 +177,7 @@ function searchCity(event)
 /************************************** 
 Add event listener to generate button
 **************************************/
-searchBtnEl.addEventListener("click", searchCity);
+var form = document.querySelector('form')
+form.addEventListener("submit", searchCity);
 
 
